952083c9cd52ecda1d8ea94716e0bbc5b7cb89f5
christian <christian@minimi.com>
2015-01-17 13:55:01 +0100

    add user

--- KnowledgeExchangeTool/routes/users.js
+++ KnowledgeExchangeTool/routes/users.js
@@ -10,9 +10,39 @@ router.get('/list', function(req, res) {
     var db = req.db;
     var collection = db.get('userCollection');
     collection.find({},{},function(e,docs){
-        res.render('list-users', {
+        res.render('users-list', {
             "userlist" : docs
         });
+    });
+});
+
+/* GET New User page. */
+router.get('/new', function(req, res) {
+    res.render('users-new', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/add', function(req, res) {
+    var db = req.db;
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    userName = 'test';
+    var collection = db.get('userCollection');
+
+    console.log('add user:'+userName);
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+    }, function (err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say adduser
+            res.location("list");
+            // And forward to success page
+            res.redirect("list");
+        }
     });
 });
 module.exports = router;
