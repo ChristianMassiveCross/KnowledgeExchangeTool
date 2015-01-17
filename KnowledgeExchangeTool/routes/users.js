09e2ff21c14c17da112ab4abc432d34d27492be7
christian <christian@minimi.com>
2015-01-17 13:08:48 +0100

    add first version of user list

--- KnowledgeExchangeTool/routes/users.js
+++ KnowledgeExchangeTool/routes/users.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+/* GET Userlist page. */
+router.get('/list', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userCollection');
+    collection.find({},{},function(e,docs){
+        res.render('list-users', {
+            "userlist" : docs
+        });
+    });
+});
 module.exports = router;
